db861e156e3cd9355442d0a02f5ce894341a87ca
Tony Simonutti <44368695+asimonutti@users.noreply.github.com>
2021-06-17 17:05:00 -0400

    Updating sample-data from 2021-05-26 (#11)
    * Updated for new folder path
    
    * Updated sample-data from AWS
    
    * Updated spacing
    
    * Only check buckets prefixed with

--- sample-data/update_avro_from_aws.sh
+++ sample-data/update_avro_from_aws.sh
@@ -15,17 +15,19 @@ echo "INFO: Checking for avro schemas from ${date} in ${bucket}"
 for path in $(aws s3 ls ${bucket}${prefix});
 do
   if [[ $path != "" ]] && [[ $path != "PRE" ]]; then
-    event_type=$(echo $path | sed 's/event_type=//g;s/\///g')
-    payloadpath=$(aws s3 ls "${bucket}${prefix}${path}${date}/" --recursive | sort | tail -n 1 | awk '{print $4}')
-    if [[ $payloadpath == "" ]]; then
-      echo "WARNING: Looks like we can't find anything for the current date ${date}, check the hour before."
-      prev_date="date=$(date -u -v-60M '+%Y-%m-%d-%H')"
-      payloadpath=$(aws s3 ls "${bucket}${prefix}${path}${prev_date}/" --recursive | sort | tail -n 1 | awk '{print $4}')
+    if [[ $path = "users"* ]]; then
+      event_type=$(echo $path | sed 's/users.//g;s/\///g')
+      payloadpath=$(aws s3 ls "${bucket}${prefix}${path}${date}/" --recursive | sort | tail -n 1 | awk '{print $4}')
       if [[ $payloadpath == "" ]]; then
-        echo "ERROR: Couldn't find data in ${prev_date} either. Exiting."
-        exit 1
+        echo "WARNING: Looks like we can't find anything for the current date ${date} in path ${path}, check the hour before."
+        prev_date="date=$(date -u -v-60M '+%Y-%m-%d-%H')"
+        payloadpath=$(aws s3 ls "${bucket}${prefix}${path}${prev_date}/" --recursive | sort | tail -n 1 | awk '{print $4}')
+        if [[ $payloadpath == "" ]]; then
+          echo "ERROR: Couldn't find data in ${prev_date} either. Exiting."
+          exit 1
+        fi
       fi
+      aws s3 cp s3://${bucket}${payloadpath} Avro/${event_type}.avro
     fi
-    aws s3 cp s3://${bucket}${payloadpath} Avro/${event_type}.avro
   fi
 done
