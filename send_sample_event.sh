3eab7a94d9e2011190b6b9de8162198d85e30774
Jeff Stutz <jeffreystutz@users.noreply.github.com>
2023-03-13 11:03:25 -0700

    Added scripts to send sample events to a specific endpoint

--- send_sample_event.sh
+++ send_sample_event.sh
@@ -1,8 +1,16 @@
-#!/bin/bash -x
+#!/bin/bash
+
+# Any code, scripts, guides, written advice ("Scripts") provided by Braze herein are not, and shall not be, a component of the Braze Services nor the Braze SDK, as defined in Braze's Master Subscription Agreement. All Scripts are provided as-is, without any warranty of any kind, express or implied. In no event shall the authors or copyright holders any Script be liable for any claim, damages, or other liability arising from or in connection with the Script.
 
 dest=$1
 sample_file=$2
 bearer=$3
+
+if [[ ! -n ${dest} || ! -s ${sample_file} ]]; then
+    echo "Usage: $0 URL Filename [Bearer Token]"
+    exit 1
+fi
+
 
 auth_header=""
 if [[ -n "$bearer" ]]; then
--- send_sample_events.sh
+++ send_sample_events.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+#Any code, scripts, guides, written advice ("Scripts") provided by Braze herein are not, and shall not be, a component of the Braze Services nor the Braze SDK, as defined in Braze's Master Subscription Agreement. All Scripts are provided as-is, without any warranty of any kind, express or implied. In no event shall the authors or copyright holders any Script be liable for any claim, damages, or other liability arising from or in connection with the Script.
+
 dest=$1
 bearer=$2
+
+if [[ -n ${dest} ]]; then
+    echo "Usage: $0 URL [Bearer Token]"
+    exit 1
+fi
+
 
 find sample-data/JSON \
      -type f \
