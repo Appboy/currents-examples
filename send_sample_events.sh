7d951b3216cad3ac436d6d8657e4cfa689329aa8
jeffreystutz <jeffreystutz@users.noreply.github.com>
2023-06-26 10:25:45 -0700

    Updated terms for scripts (#22)

--- send_sample_events.sh
+++ send_sample_events.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Any code, scripts, guides, written advice ("Scripts") provided by Braze herein are not, and shall not be, a component of the Braze Services nor the Braze SDK, as defined in Braze's Master Subscription Agreement. All Scripts are provided as-is, without any warranty of any kind, express or implied. In no event shall the authors or copyright holders any Script be liable for any claim, damages, or other liability arising from or in connection with the Script.
+# Any code, scripts, guides, recommendations, and/or written advice (generally, "Scripts") provided by Braze herein are not, and shall not be considered a component of the Braze Services nor the Braze SDK, as defined in Braze's Main Subscription Agreement. All Scripts are provided as-is, without any warranty of any kind, express or implied. In no event shall Braze be liable for any claim, damages, or other liability arising from or in connection with any such Script.
 
 dest=$1
 bearer=$2
