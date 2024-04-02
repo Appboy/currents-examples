8d95625bb8fc749593082fa8eb1024d98d52fd66
flybonzai <flybonzai@gmail.com>
2024-04-02 13:03:42 -0400

    Use brazedev profile

--- sample-data/update_avro_from_aws.sh
+++ sample-data/update_avro_from_aws.sh
@@ -12,22 +12,22 @@ prefix=$2
 
 echo "INFO: Checking for avro schemas from ${date} in ${bucket}"
 
-for path in $(aws s3 ls ${bucket}${prefix});
+for path in $(aws s3 --profile brazedev ls ${bucket}${prefix});
 do
   if [[ $path != "" ]] && [[ $path != "PRE" ]]; then
     if [[ $path = "event_type=users"* ]]; then
       event_type=$(echo $path | sed 's/event_type=*//g;s/\///g')
-      payloadpath=$(aws s3 ls "${bucket}${prefix}${path}${date}/" --recursive | sort | tail -n 1 | awk '{print $4}')
+      payloadpath=$(aws s3 --profile brazedev ls "${bucket}${prefix}${path}${date}/" --recursive | sort | tail -n 1 | awk '{print $4}')
       if [[ $payloadpath == "" ]]; then
         echo "WARNING: Looks like we can't find anything for the current date ${date} in path ${path}, check the hour before."
         prev_date="date=$(date -u -v-60M '+%Y-%m-%d-%H')"
-        payloadpath=$(aws s3 ls "${bucket}${prefix}${path}${prev_date}/" --recursive | sort | tail -n 1 | awk '{print $4}')
+        payloadpath=$(aws s3 --profile brazedev ls "${bucket}${prefix}${path}${prev_date}/" --recursive | sort | tail -n 1 | awk '{print $4}')
         if [[ $payloadpath == "" ]]; then
           echo "ERROR: Couldn't find data in ${prev_date} either. Exiting."
           exit 1
         fi
       fi
-      aws s3 cp s3://${bucket}${payloadpath} Avro/${event_type}.avro
+      aws s3 --profile brazedev cp s3://${bucket}${payloadpath} Avro/${event_type}.avro
     fi
   fi
 done
